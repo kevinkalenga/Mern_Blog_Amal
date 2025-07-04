9c23b085a62c7dc8e328db68d9ed2a203ec23fa4
kevin kalenga <kevinkalenga10@gmail.com>
2025-07-04 15:06:55 +0200

    Connexion à la base des données

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("Connexion reussie")
+}).catch((err) => {
+    console.log(err)
+})
+
+
+
 
 const app = express()
-
 app.listen(3000, () => {
     console.log("serveur ok sur le port 3000")
 })
