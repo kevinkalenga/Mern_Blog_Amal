27a9eda828e409c9f4c80bc1aefa665acf42ff90
kevin kalenga <kevinkalenga10@gmail.com>
2025-07-07 15:23:34 +0200

    Ajout des fonctionalités permettant à l'inscription de l'utilisateur

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,7 +1,56 @@
-import { Link } from "react-router-dom";
-import { Label, TextInput, Button } from 'flowbite-react';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {Alert, Label, TextInput, Spinner, Button } from 'flowbite-react';
+
 
 export default function SignUp() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({})
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+     setFormData({
+       ...formData,
+       [e.target.id]:e.target.value.trim()
+     })
+  }
+
+  
+
+  const handleSubmit = async(e) => {
+     e.preventDefault()
+     if(!formData.username || !formData.email || !formData.password) {
+        return setErrorMessage('Tous les champs sont requis')
+     }
+
+     try {
+      setLoading(true)
+      setErrorMessage(null)
+       const res = await fetch('/api/auth/signup', {
+         method: 'POST',
+         headers: {
+          'Content-Type': 'application/json'
+         },
+         body: JSON.stringify(formData)
+       })
+       const data = await res.json(); 
+       if(data.success === false) {
+         return setErrorMessage(data.message)
+       }
+       setLoading(false)
+       if(res.ok) {
+         navigate('/sign-in')
+       }
+     } catch (error) {
+       setErrorMessage(error.message)
+       setLoading(false)
+     }
+
+  }
+
+ 
+  
   return (
     <div className="min-h-screen mt-20 bg-gray-100">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -22,24 +71,25 @@ export default function SignUp() {
 
         {/* right */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4 bg-white p-6 rounded-lg shadow-md">
+          <form className="flex flex-col gap-4
+             bg-white p-6 rounded-lg shadow-md" onSubmit={handleSubmit}>
             <div>
               <Label value="Votre nom" />
-              <TextInput type="text" placeholder="Votre nom" id="username" />
+              <TextInput type="text" placeholder="Votre nom" id="username" onChange={handleChange} />
             </div>
             <div>
               <Label value="Votre email" />
-              <TextInput type="email" placeholder="name@company.com" id="email" />
+              <TextInput type="email" placeholder="name@company.com" id="email" onChange={handleChange} />
             </div>
             <div>
               <Label value="Votre mot de passe" />
-              <TextInput type="password" placeholder="Mot de passe" id="password" />
+              <TextInput type="password" placeholder="Mot de passe" id="password" onChange={handleChange} />
             </div>
 
             {/* ✅ Le bouton dans une div stylée */}
              <div className="bg-white p-4 rounded-md shadow-md">
                      <Button color="blue" type="submit">
-                          Connectez-vous
+                          Inscription
                       </Button>
              </div>
 
