c91df2eee13485bc2dcd8cc9d102f6e8a4c527e3
kevin kalenga <kevinkalenga10@gmail.com>
2025-07-16 16:41:50 +0200

    Ajout de contenu de la page profile

--- frontend/src/components/DashProfile.jsx
+++ frontend/src/components/DashProfile.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import { useSelector } from "react-redux";
+import { TextInput, Button } from "flowbite-react";
 
 export default function DashProfile () {
-  return (
-    <div>DashProfile</div>
+  
+    const {currentUser} = useSelector((state) => state.user)
+    console.log(currentUser)
+  
+  
+    return (
+    <div className="max-w-lg mx-auto p-3 w-full">
+       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
+       <form className="flex flex-col gap-4">
+           <div className="h-32 w-32 cursor-pointer self-center 
+           shadow-md overflow-hidden rounded-full">
+              <img src={currentUser.profilePicture} alt="user" 
+              className="rounded-full h-full w-full object-cover border-[lightgray]"
+              
+              />
+           </div>
+           <TextInput 
+            type="text"
+            id="username"
+            placeholder="username"
+            defaultValue={currentUser.username}
+           />
+           <TextInput 
+            type="email"
+            id="email"
+            placeholder="email"
+            defaultValue={currentUser.email}
+           />
+           <TextInput 
+            type="password"
+            id="password"
+            placeholder="password"
+            defaultValue={currentUser.password}
+           />
+            <Button color="blue" type="submit">
+                    Mettre à jour
+           </Button>
+           
+       </form>
+       <div className="flex justify-between text-red-500 mt-5">
+         <span className="cursor-pointer">Supprimer le compte</span>
+         <span className="cursor-pointer">Déconnexion</span>
+       </div>
+    </div>
   )
 }
