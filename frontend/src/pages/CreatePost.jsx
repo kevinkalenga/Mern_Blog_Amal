bee2b17c81dcb1de017a825aca63f8df0d1f511f
kevin kalenga <kevinkalenga10@gmail.com>
2025-07-25 16:37:10 +0200

    Ajouter des fonctionalités permettant de telecharger une image dans le composant Createpost

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,10 +1,63 @@
-import { Button, FileInput, Select, TextInput } from "flowbite-react";
+import { Alert, Button, FileInput, Select, TextInput } from "flowbite-react";
 import { useState } from 'react';
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css';
 
+// elements importés depuis dashProfile
+import {getStorage, ref, getDownloadURL, uploadBytesResumable} from 'firebase/storage'
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import {app} from '../firebase'
+
 export default function CreatePost() {
      const [content, setContent] = useState('');
+     const [file, setFile] = useState(null);
+     const [imageUploadProgress, setImageUploadProgress] = useState(null)
+     const [imageUploadError, setImageUploadError] = useState(null)
+      const [formData, setFormData] = useState({});
+
+      const handleUploadImage = async () => {
+            try {
+                   if(!file) {
+                      setImageUploadError("Selectionner une image svp");
+                        return;
+                    }
+                     setImageUploadError(null);
+                       
+                       const storage = getStorage(app)
+                       const fileName = new Date().getTime() + '-' + file.name;
+                       const storageRef = ref(storage, fileName)
+                       const uploadTask = uploadBytesResumable(storageRef, file);
+
+                       uploadTask.on(
+                            'state_changed',
+                            (snapshot) => {
+                             const progress = 
+                             (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                             setImageUploadProgress(progress.toFixed(0));
+                            },
+
+                             (error) => {
+                                setImageUploadError('Echec du télechargement')
+                                setImageUploadProgress(null)
+                            },
+                            () => {
+                                      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                                       setImageUploadProgress(null);
+                                       setImageUploadError(null)
+                                       setFormData({...formData, image: downloadURL})
+                                      })
+                                }
+                            
+                       )
+
+            } catch (error) {
+                setImageUploadError('Le téchargement a echoué')
+                setImageUploadProgress(null)
+                console.log(error)
+            }
+      }
+
     
     return (
         <div className="min-h-screen p-3 mx-auto max-w-3xl">
@@ -23,12 +76,39 @@ export default function CreatePost() {
                 </div>
                 <div className="flex gap-4 items-center 
                     justify-between border-4 border-teal-500 border-dotted p-3">
-                    <FileInput type='file' accept="image/*" />
-                     <Button color="blue" type="submit">
-                          Telecharger l'image
+                    <FileInput type='file' accept="image/*" 
+                    onChange={(e)=>setFile(e.target.files[0])}
+                    />
+                     <Button onClick={handleUploadImage} disabled={imageUploadProgress} color="blue" type="button">
+                          
+                        {
+                            imageUploadProgress ? (
+                                 <div className="w-16 h-16">
+                                    <CircularProgressbar 
+                                       value={imageUploadProgress}
+                                       text={`${imageUploadProgress || 0}%`}
+                                    />
+                                 </div>
+                            ):(
+                              "Telecharger l'image "   
+                            )
+                        }
+                      
                       </Button>
                 
                 </div>
+                {
+                    imageUploadError && (
+                        <Alert color="failure">
+                            {imageUploadError}
+                        </Alert>
+                    )
+                }
+                {
+                    formData.image && (
+                        <img src={formData.image} alt="upload" className="w-full h-72 object-cover" />
+                    )
+                }
          
                  {/* <ReactQuill theme='snow' placeholder="Ecrit quelque chose..."
                  className="h-72 mb-12" 
