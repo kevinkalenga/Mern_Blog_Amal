a17a8f973b059225c16ddf59f80139f3eeb21e76
kevin kalenga <kevinkalenga10@gmail.com>
2025-07-28 01:16:23 +0200

    Ajout des fonctionalités permettant à implementer la pagination

--- frontend/src/components/DashPosts.jsx
+++ frontend/src/components/DashPosts.jsx
@@ -77,7 +77,7 @@ import { Link } from "react-router-dom";
 export default function DashPosts() {
     const { currentUser } = useSelector((state) => state.user);
     const [userPosts, setUserPosts] = useState([]);
-    console.log(userPosts)
+    const [showMore, setShowMore] = useState(true)
 
     useEffect(() => {
         const getUserPosts = async () => {
@@ -88,6 +88,9 @@ export default function DashPosts() {
                 const data = await res.json();
                 if (res.ok) {
                     setUserPosts(data.posts);
+                    if(data.post.length < 9) {
+                        setShowMore(false)
+                    }
                 }
             } catch (error) {
                 console.log(error.message)
@@ -99,6 +102,24 @@ export default function DashPosts() {
         }
     }, [currentUser._id]);
 
+    const handleShowMore = async() => {
+        const startIndex = userPosts.length;
+        try {
+            const res = await fetch(`/api/post/getposts?userid=${currentUser._id}&startIndex=${startIndex}`)
+            const data = await res.json();
+            if(res.ok) {
+                setUserPosts((prev) => [...prev, ...data.posts]);
+                if(data.posts.length < 9) {
+                    setShowMore(false)
+                }
+            }
+        } catch (error) {
+             console.log(error)
+        }
+    }
+    
+    
+    
     return(
          <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-slate-700 dark:scrollbar-thumb-slate-500">
             {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -152,6 +173,13 @@ export default function DashPosts() {
                             ))
                         }
                     </Table>
+                    {
+                        showMore && (
+                            <button className="w-full text-teal-500 self-center text-sm py-7" onClick={handleShowMore}>
+                                  Affiche + 
+                            </button>
+                        )
+                    }
 
                 </>
             ) : (
